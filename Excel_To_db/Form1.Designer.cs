738acfccb9dfac57d40487127b9a06c11da89793
Rajesh Veeramuthu <140933435+RajeshVeeramuthu@users.noreply.github.com>
2026-05-13 19:39:53 +0530

    Add files via upload

---
+++ Excel_To_db/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace excel_to_db
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ServerTextBox = new System.Windows.Forms.TextBox();
+            this.DatabaseTextBox = new System.Windows.Forms.TextBox();
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ExcelFilePathTextBox = new System.Windows.Forms.TextBox();
+            this.ExportButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ServerTextBox
+            // 
+            this.ServerTextBox.Location = new System.Drawing.Point(100, 36);
+            this.ServerTextBox.Name = "ServerTextBox";
+            this.ServerTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ServerTextBox.TabIndex = 0;
+            // 
+            // DatabaseTextBox
+            // 
+            this.DatabaseTextBox.Location = new System.Drawing.Point(100, 62);
+            this.DatabaseTextBox.Name = "DatabaseTextBox";
+            this.DatabaseTextBox.Size = new System.Drawing.Size(100, 20);
+            this.DatabaseTextBox.TabIndex = 1;
+            // 
+            // UsernameTextBox
+            // 
+            this.UsernameTextBox.Location = new System.Drawing.Point(100, 88);
+            this.UsernameTextBox.Name = "UsernameTextBox";
+            this.UsernameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.UsernameTextBox.TabIndex = 2;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(100, 114);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PasswordTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Server Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Database Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "User Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Password";
+            // 
+            // ExcelFilePathTextBox
+            // 
+            this.ExcelFilePathTextBox.Location = new System.Drawing.Point(15, 157);
+            this.ExcelFilePathTextBox.Name = "ExcelFilePathTextBox";
+            this.ExcelFilePathTextBox.Size = new System.Drawing.Size(198, 20);
+            this.ExcelFilePathTextBox.TabIndex = 8;
+            // 
+            // ExportButton
+            // 
+            this.ExportButton.Location = new System.Drawing.Point(68, 203);
+            this.ExportButton.Name = "ExportButton";
+            this.ExportButton.Size = new System.Drawing.Size(113, 46);
+            this.ExportButton.TabIndex = 9;
+            this.ExportButton.Text = "Import";
+            this.ExportButton.UseVisualStyleBackColor = true;
+            this.ExportButton.Click += new System.EventHandler(this.ExportButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(206, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 27);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ExportButton);
+            this.Controls.Add(this.ExcelFilePathTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.UsernameTextBox);
+            this.Controls.Add(this.DatabaseTextBox);
+            this.Controls.Add(this.ServerTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ServerTextBox;
+        private System.Windows.Forms.TextBox DatabaseTextBox;
+        private System.Windows.Forms.TextBox UsernameTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ExcelFilePathTextBox;
+        private System.Windows.Forms.Button ExportButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Excel_To_db/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using MySql.Data.MySqlClient;
+
+namespace excel_to_db
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ExportButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string excelFilePath = ExcelFilePathTextBox.Text;
+                string connectionString = GetConnectionString(); // Move connection string to a separate method
+
+                Excel.Application excelApp = new Excel.Application();
+                Excel.Workbook workbook = excelApp.Workbooks.Open(excelFilePath);
+                Excel.Worksheet worksheet = (Excel.Worksheet)workbook.Sheets[1];
+
+                using (var connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (var transaction = connection.BeginTransaction()) // Use a transaction for better reliability
+                    {
+                        for (int row = 2; row <= worksheet.UsedRange.Rows.Count; row++)
+                        {
+                            string col1Value = ((Excel.Range)worksheet.Cells[row, 1]).Text;
+                            string col2Value = ((Excel.Range)worksheet.Cells[row, 2]).Text;
+                            string col3Value = ((Excel.Range)worksheet.Cells[row, 3]).Text;
+                            string col4Value = ((Excel.Range)worksheet.Cells[row, 4]).Text;
+                            string col5Value= ((Excel.Range)worksheet.Cells[row, 5]).Text;
+                            string col6Value= ((Excel.Range)worksheet.Cells[row, 6]).Text;
+                            string col7Value = ((Excel.Range)worksheet.Cells[row, 7]).Text;
+                            string col8Value= ((Excel.Range)worksheet.Cells[row, 8]).Text;
+
+                            string insertQuery = "INSERT INTO student_details (Reg_no,Name,Course,Class,emailid,acd_start,acd_end,status) VALUES (@col1, @col2,@col3, @col4,@col5, @col6,@col7,@col8)";
+                            using (var command = new MySqlCommand(insertQuery, connection))
+                            {
+                                command.Parameters.AddWithValue("@col1", col1Value);
+                                command.Parameters.AddWithValue("@col2", col2Value);
+                                command.Parameters.AddWithValue("@col3", col3Value);
+                                command.Parameters.AddWithValue("@col4", col4Value);
+                                command.Parameters.AddWithValue("@col5", col5Value);
+                                command.Parameters.AddWithValue("@col6", col6Value);
+                                command.Parameters.AddWithValue("@col7", col7Value);
+                                command.Parameters.AddWithValue("@col8", col8Value);
+                                command.ExecuteNonQuery();
+                            }
+                        }
+                        transaction.Commit(); // Commit the transaction if all inserts succeed
+                    }
+                    MessageBox.Show("Data inserted successfully.");
+                }
+
+                // Cleanup Excel objects
+                workbook.Close(false);
+                excelApp.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+                GC.Collect();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error");
+            }
+            DatabaseTextBox.Text =String.Empty;
+            ServerTextBox.Text = String.Empty;
+            UsernameTextBox.Text = String.Empty;
+            PasswordTextBox.Text = String.Empty;
+            ExcelFilePathTextBox.Text = String.Empty;
+        }
+
+        private string GetConnectionString()
+        {
+            // Construct and return the MySQL connection string
+            return "Server=localhost;Database=sample;User Id=root;Password=9557;";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Select an Excel File";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                ExcelFilePathTextBox.Text = openFileDialog.FileName;
+            }
+        }
+    }
+}
